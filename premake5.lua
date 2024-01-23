cac93414a85c6f4cb84b4b3463a0a7ba81ce5785
PikaMax <pikazxzxcc112@gmail.com>
2024-01-23 20:35:03 +0900

    add premake

---
+++ premake5.lua
@@ -0,0 +1,43 @@
+project "GLFW"
+	kind "StaticLib"
+	language "C"
+
+	targetdir ("bin/" .. outputDir .. "%{prj.name}")
+	objdir ("bin-int/" .. outputDir .. "%{prj.name}")
+
+	files
+	{
+		"include/GLFW/glfw3.h",
+		"include/GLFW/glfw3native.h",
+		"src/glfw_config.h",
+		"src/context.c",
+		"src/init.c",
+		"src/input.c",
+		"src/monitor.c",
+		"src/vulkan.c",
+		"src/window.c"
+	}
+
+	filter "system:windows"
+		staticruntime "On"
+		systemversion "latest"
+
+		files
+		{
+			"src/win32_init.c",
+            "src/win32_joystick.c",
+			"src/win32_monitor.c",
+			"src/win32_time.c",
+			"src/win32_thread.c",
+			"src/win32_window.c",
+			"src/win32_module.c",
+			"src/wgl_context.c",
+			"src/egl_context.c",
+			"src/osmesa_context.c"
+		}
+
+		defines
+		{
+			"_GLFW_WIN32"
+
+		}
